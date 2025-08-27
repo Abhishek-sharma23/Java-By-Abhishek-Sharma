786c1c7ad330649346cb1e6359ec12e910fcd60f
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-08-27 21:01:51 +0530

    Add files via upload

---
+++ Number Pattern/n2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class n29
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print(i+j==n+1||i==n?j+" ":"  ");
+            }
+            System.out.println();
+        }
+    }
+}
