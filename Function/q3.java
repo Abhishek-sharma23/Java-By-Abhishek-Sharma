4ab138ac526ed7c584523a411f9789975a33a427
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-09-03 10:17:11 +0530

    Add files via upload

---
+++ Function/q3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class q3
+{
+
+    public static int mul(int a, int b)
+    {
+        return a*b;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.print(mul(a,b));
+    }
+}
