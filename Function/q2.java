f41eba39c8f1dc16f94bf15cf7e761357f642bce
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-09-03 10:16:52 +0530

    Add files via upload

---
+++ Function/q2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class q2
+{
+    public static void num(int a, int b)
+    {
+        System.out.print(a+b);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        num(a,b);
+    }
+}
