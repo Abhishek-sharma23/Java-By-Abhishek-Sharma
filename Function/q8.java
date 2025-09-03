84e520c3beadec286520ed0d05515e436f8fa858
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-09-03 11:03:37 +0530

    Add files via upload

---
+++ Function/q8.java
@@ -0,0 +1,13 @@
+class q8
+{
+    public static void Circumference(double n)
+    {
+        double a=n*2;
+        System.out.print(a*3.14);
+    }
+    public static void main(String[] args)
+    {   
+        double n=5;
+        Circumference(n);
+    }
+}
