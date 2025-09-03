686bdcd5829d79ea9118a98fc91562296f0991e7
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-09-03 10:16:20 +0530

    Add files via upload

---
+++ Function/q1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class q1
+{
+    public static void Name(String n)
+    {
+        System.out.print(n);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        Name(n);
+    }
+}
