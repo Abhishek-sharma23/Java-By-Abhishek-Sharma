6e0186ce5d0fdf7ce920a91d17e49ec1023affa1
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2025-11-05 23:10:48 +0530

    Add files via upload

---
+++ Array/q2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class q2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            num[i]=sc.nextInt();
+        }
+        int x=sc.nextInt();
+        for(int i=0;i<num.length;i++)
+        {
+             if(num[i]==x)
+             {
+                System.out.print(i);
+             }
+        }
+    }
+}
