5f0b27b973508666c6dafac0a7b41bd3a96fc7fd
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-01-29 10:02:10 +0530

    Add files via upload

---
+++ String/q19.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class q19
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter String - ");
+        String s=sc.nextLine();
+        System.out.print(s.substring(0,5));
+    }
+}
