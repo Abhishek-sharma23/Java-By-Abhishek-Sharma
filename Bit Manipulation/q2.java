258ad6920d52dda90306fc1faba4b30184b909a5
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-01-29 10:17:33 +0530

    Add files via upload

---
+++ Bit Manipulation/q2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class q2
+{
+    public static void main(String[] args)      //OR Operator
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter 1st Number - ");
+        int n1=sc.nextInt();
+        System.out.print("Enter 2nd Number - ");
+        int n2=sc.nextInt();
+        System.out.println((n1|n2));
+    }
+}
