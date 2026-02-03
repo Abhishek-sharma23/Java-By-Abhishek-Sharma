11a7821eb26c9f055651fb1dca8888e6b2c52307
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-02-03 09:26:02 +0530

    Add files via upload

---
+++ Bit Manipulation/q4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class q4
+{
+    public static void main(String[] args)      //Binary One's Complement Operator
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter 1st Number - ");
+        int n=sc.nextInt();
+        System.out.println(~n);
+    }
+}
