a4f61f0979225d5b29df064eb96f16ae4f31cf85
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-02-11 08:27:27 +0530

    Add files via upload

---
+++ Basic/q7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class q7
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Limit - ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
