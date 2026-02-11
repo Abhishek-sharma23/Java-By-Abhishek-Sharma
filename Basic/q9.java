0d6d0eec2af4a41189e48d09a9fa74e201dd3ca2
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-02-11 08:44:21 +0530

    Add files via upload

---
+++ Basic/q9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class q9
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number To Print Table - ");
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+"x"+i+"="+(i*n));
+        }
+    }
+}
