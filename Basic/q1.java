6773f4d2910f94a529803a281eed1c157c84b6c4
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-02-11 08:25:09 +0530

    Add files via upload

---
+++ Basic/q1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class q1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter 1st Number - ");
+        int a=sc.nextInt();
+        System.out.print("Enter 2nd Number - ");
+        int b=sc.nextInt();
+        System.out.print("Output Is - "+(a+b));
+    }
+}
