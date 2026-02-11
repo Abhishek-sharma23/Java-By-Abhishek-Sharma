a43198074474e695e68ea89bf1802ba5f52dddbd
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-02-11 08:25:53 +0530

    Add files via upload

---
+++ Basic/q2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class q2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of P - ");
+        int p=sc.nextInt();
+        System.out.print("Enter the value of R - ");
+        int r=sc.nextInt();
+        System.out.print("Enter the value of T - ");
+        int t=sc.nextInt();
+        System.out.print("Simple Interest is - "+(p+r+t)/10);
+    }
+}
